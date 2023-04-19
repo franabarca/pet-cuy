b8efb6e96c503c8926665f9131050c25c5ce5f45
Wesret <101572510+Wesret@users.noreply.github.com>
2023-04-19 00:14:00 -0400

    19-04-23 00:13
    implementacion page registro

--- src/app/inicio-sesion/registro/registro.page.ts
+++ src/app/inicio-sesion/registro/registro.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, AlertController } from '@ionic/angular';
 import { MenuService } from '../../menu.service';
 import { Router } from '@angular/router';
 
@@ -14,46 +14,28 @@ import { Router } from '@angular/router';
 })
 export class RegistroPage implements OnInit {
 
-  registerForm: FormGroup;
-  isSubmitted = false;
-
-  constructor(private menuService: MenuService, private router: Router, public formBuilder: FormBuilder) {}
+  constructor(private menuService: MenuService, private router: Router, public alertController: AlertController) {}
 
   ionViewDidEnter() {
     this.menuService.ocultarMenu();
   }
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      rut: ['', [Validators.required, Validators.pattern('[a-z]'), Validators.maxLength(8)], Validators.minLength(8)],
-      nombre: ['', [Validators.required, Validators.pattern('[a-z]')]],
-      apellido: ['', [Validators.required, Validators.pattern('[a-z]')]],
-      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9]+.+[a-z]')]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    })
+    
   }
 
-  get errorControl(){
-    return this.registerForm.controls;
-  }
-
-  InicioSesion(){
+  inicioSesion(){
+    this.presentAlert("Felicidades!","Registro Completado");
     this.router.navigate(['/login']);
   }
 
-  submitForm(){
-    this.isSubmitted = true;
+  async presentAlert(titulo: string, msg: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: msg,
+      buttons: ['OK'],
+    });
 
-    if (!this.registerForm.valid){
-      console.log("")
-      return false;
-
-    }else{
-      console.log(this.registerForm.value)
-      this.router.navigate(['/login']);
-      return;
-    }
-
+    await alert.present();
 
   }
-
 }
