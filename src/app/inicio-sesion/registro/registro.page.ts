e284bb12f4ed6235e85318a501c75a36d6b25123
Wesret <101572510+Wesret@users.noreply.github.com>
2023-04-25 18:17:35 -0400

    25-04-2023 18:17
    Validaciones Registro

--- src/app/inicio-sesion/registro/registro.page.ts
+++ src/app/inicio-sesion/registro/registro.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { IonicModule, AlertController } from '@ionic/angular';
 
 import { Router } from '@angular/router';
@@ -14,6 +14,35 @@ import { Router } from '@angular/router';
 })
 export class RegistroPage implements OnInit {
 
+  /*VALIDAR RECUPERAR CONTRASEÑA FORM*/
+  forgotForm = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(9), Validators.pattern('[0-9A-Za-z]+'), this.rutValidator()]),
+    nombre: new FormControl('',),
+    apellido: new FormControl('',),
+    email: new FormControl('',),
+
+  });
+
+  getErrorMessage(controlName: string) {
+    const control = this.forgotForm.get(controlName);
+    if (control?.hasError('required')) {
+      return 'El campo es requerido.';
+    }
+    if (control?.hasError('minlength')) {
+      return `El campo debe tener al menos ${control?.errors?.['minlength']?.requiredLength} caracteres.`;
+    }
+    if (control?.hasError('maxlength')) {
+      return `El campo no puede tener más de ${control?.errors?.['maxlength']?.requiredLength} caracteres.`;
+    }
+    if (control?.hasError('pattern')) {
+      return 'El campo solo puede contener letras y números.';
+    }
+    if (control?.hasError('invalidRut')) {
+      return 'Debe ingresar un rut real.';
+    }
+    return '';
+  }
+
   constructor( private router: Router, public alertController: AlertController) {}
 
 
@@ -24,6 +53,66 @@ export class RegistroPage implements OnInit {
   inicioSesion(){
     this.presentAlert("Felicidades!","Registro Completado");
     this.router.navigate(['/login']);
+  }
+
+  submitForm() {
+    if (this.forgotForm.valid) {
+      this.presentAlert("Felicitaciones","Se ha registrado completamente");
+      this.router.navigate(['/login']);
+    } else {
+      this.presentAlert("Error","Completa correctamente los campos.");
+    }
+  }
+
+  rutValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const rut = control.value;
+      if (!rut) {
+        return null;
+      }
+      const isValid = this.validarRut(rut);
+      return isValid ? null : { 'invalidRut': true };
+    };
+  }
+
+  validarRut(rut: string): boolean {
+    rut = rut.toUpperCase();
+    rut = rut.replace("-", "");
+    rut = rut.replace(".", "");
+    const aux = rut.slice(0,-1);
+    let dv: string = rut.slice(-1);
+  
+    if (dv === 'K') {
+      dv = '10';
+    } else if (!dv.match(/^\d+$/)) {
+      return false;
+    }
+  
+    if (!aux.match(/^\d+$/)) {
+      return false;
+    }
+  
+    const revertido = Array.from(aux, Number).reverse();
+    const factors = [2, 3, 4, 5, 6, 7];
+    let s = 0;
+    let f = 0;
+  
+    for (let i = 0; i < revertido.length; i++) {
+      f = factors[i % factors.length];
+      s += revertido[i] * f;
+    }
+  
+    let res = (11 - (s % 11)).toString();
+    
+    if (res === '11') {
+      res = '0';
+    }
+  
+    if (res === dv) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   async presentAlert(titulo: string, msg: string) {
