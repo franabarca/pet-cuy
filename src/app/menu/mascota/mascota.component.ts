74269b685f4b3ae74bbba5669f75c1a1b132c56f
franabarca <101573112+franabarca@users.noreply.github.com>
2023-05-16 19:52:18 -0400

    pequeños cambbios

---
+++ src/app/menu/mascota/mascota.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota',
+  templateUrl: './mascota.component.html',
+  styleUrls: ['./mascota.component.scss'],
+})
+export class MascotaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
